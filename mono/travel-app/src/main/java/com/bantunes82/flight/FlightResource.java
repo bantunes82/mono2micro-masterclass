83209a533a60f9c48779bed1e66a7ae3a3a72337
bantunes82 <bantunes82@gmail.com>
2023-10-27 08:46:11 +0200

    Segunda aula

--- mono/travel-app/src/main/java/com/bantunes82/flight/FlightResource.java
+++ mono/travel-app/src/main/java/com/bantunes82/flight/FlightResource.java
@@ -22,6 +22,13 @@ public class FlightResource {
         return Flight.findById(id);
     }
 
+    @GET
+    @Path("findByTravelOrderId")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId){
+        return Flight.findByTravelOrderId(travelOrderId);
+    }
+
     @Transactional
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
--- mono/travel-app/src/main/java/com/bantunes82/travelorder/TravelOrderResource.java
+++ mono/travel-app/src/main/java/com/bantunes82/travelorder/TravelOrderResource.java
@@ -1,18 +1,39 @@
 package com.bantunes82.travelorder;
 
+import com.bantunes82.flight.Flight;
+import com.bantunes82.flight.FlightResource;
+import com.bantunes82.hotel.Hotel;
+import com.bantunes82.hotel.HotelResource;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("travelorder")
 public class TravelOrderResource {
 
+    @Inject
+    FlightResource flightResource;
+
+    @Inject
+    HotelResource hotelResource;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<TravelOrder> orders(){
-        return TravelOrder.listAll();
+    public List<TravelOrderDTO> orders(){
+        return TravelOrder.<TravelOrder>listAll()
+                .stream()
+                .map(order -> TravelOrderDTO.of(
+                                order,
+                                flightResource.findByTravelOrderId(order.id),
+                                hotelResource.findByTravelOrderId(order.id)
+                            )
+                )
+                .collect(Collectors.toList());
+
     }
 
     @GET
@@ -26,9 +47,21 @@ public class TravelOrderResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public TravelOrder newTravelOrder(TravelOrder order){
+    public TravelOrder newTravelOrder(TravelOrderDTO orderDto){
+        TravelOrder order = new TravelOrder();
         order.id = null;
         order.persist();
+
+        Flight flight = new Flight();
+        flight.fromAirport = orderDto.getFromAirport();
+        flight.toAirport = orderDto.getToAirport();
+        flight.travelOrderId = order.id;
+        flightResource.newFlight(flight);
+
+        Hotel hotel = new Hotel();
+        hotel.nights = orderDto.getNights();
+        hotel.travelOrderId = order.id;
+        hotelResource.newHotel(hotel);
 
         return order;
     }
